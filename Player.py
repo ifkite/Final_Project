85b6669506066cf24d0a7d10a8f68d626210564d
lemonround1995 <35852552+lemonround1995@users.noreply.github.com>
2018-05-02 21:15:21 -0500

    initial version incompleted

--- Player.py
+++ Player.py
@@ -1,19 +1,21 @@
-# The player class
+"""The player class"""
+import numpy as np
 
 class Player:
-    def __init__(self, player_id, player_data):
-        self.player_id = player_id
-        self.player_nm = player_data[player_id]
-        self.team = player_data[player_id]
-        self.performance_mean = self.cal_performance_mean()
-        self.performance_var = self.cal_performance_var()
-        self.prob_be_chosen = self.cal_prob_be_chosen()
 
-    def cal_performance_mean(self):
-        pass
+    def __init__(self, playerNM, player_performance_dataset):
+        self.playerNM = playerNM
+        self.performance = player_performance_dataset[playerNM]
+        self.performance_mean = None
+        self.performance_std = None
+        self.prob_being_chosen = None
 
-    def cal_performance_var(self):
-        pass
+    def cal_performance_mean_std(self):
+        weight = [0.6, 0.2, 0.1, 0.1, 0.1, -0.05, -0.05]
+        performance_scores = np.sum(self.performance[:, 0:7] * weight, axis=1)
+        self.performance_mean = np.mean(performance_scores)
+        self.performance_std = np.std(performance_scores)
 
-    def cal_prob_be_chosen(self):
-        pass
+    def cal_prob_being_chosen(self, team_playing_time):
+        player_playing_time = np.sum(self.performance[:, 7])
+        self.prob_being_chosen = player_playing_time / team_playing_time
--- data_tools.py
+++ data_tools.py
@@ -1,1 +1,27 @@
-# This module is used to process the data files of players and teams
+"""This module is used to process the data files of players and teams"""
+
+import numpy as np
+
+
+def read_dataset(input_file):
+    performance_dataset = np.genfromtxt("nba_data.csv", dtype=np.float, delimiter=",",
+                                        skip_header=1, usecols=(2, 3, 4, 5, 6, 7, 8, 9))
+    playerNM_dataset = np.genfromtxt("nba_data.csv", dtype=str, delimiter=",",
+                                     skip_header=1, usecols=1)
+    team_dataset = np.genfromtxt("nba_data.csv", dtype=str, delimiter=",",
+                                 skip_header=1, usecols=10)
+
+    playerNM_unique = np.unique(playerNM_dataset)
+    player_performance_dataset = {}
+    for player in playerNM_unique:
+        performance = performance_dataset[np.where(playerNM_dataset == player)]
+        player_performance_dataset[player] = performance
+
+    teamNM_unique = np.unique(team_dataset)
+    team_player_time_dataset = {}
+    for team in teamNM_unique:
+        players = np.unique(playerNM_dataset[np.where(team_dataset == team)])
+        team_playing_time = np.sum(performance_dataset[np.where(team_dataset == team)][:, 7])
+        team_player_time_dataset[team] = {"players": players, "team_playing_time": team_playing_time}
+
+    return player_performance_dataset, team_player_time_dataset
