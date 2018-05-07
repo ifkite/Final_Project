298fda42f4b23c5b01578662545c1b4062255549
lemonround1995 <35852552+lemonround1995@users.noreply.github.com>
2018-05-06 21:45:46 -0500

    Match class
    Add docstring to the initial version

--- Match.py
+++ Match.py
@@ -1,34 +1,49 @@
 """the match class"""
 
-import Player
-import Team
+from Player import Player
+from Team import Team
 import numpy as np
 
 
 class Match:
 
     def __init__(self, teamA, teamB):
-        self.teamA = teamA  # team object
+        """
+
+        :param teamA: team object
+        :param teamB: team object
+        """
+        self.teamA = teamA
         self.teamB = teamB
 
     def fight_predict_champion(self):
+        """Predict the champion of this match.
+
+        :return: winner: winner team object
+        """
         team_vs_list = [self.teamA, self.teamB]
+
+        # For each team, estimate the team performance score and add it to team_performance_list
         team_performance_list = []
         for team in team_vs_list:
+            # choose 5 players to fight for the match
             five_player_list = team.choose_players()
+            # estimate team performance score and add it to the list
             team_performance = team.estimate_team_performance(five_player_list)
             team_performance_list.append(team_performance)
 
-        teamA_performance = team_performance_list[0]
-        teamB_performance = team_performance_list[1]
+        # The team with the higher score wins the match
+        # If teamA_performace = teamB_performance, then predict again
+        teamA_performance = team_performance_list[0][0]
+        teamB_performance = team_performance_list[1][0]
         if teamA_performance > teamB_performance:
             winner = self.teamA
-            print(self.teamA.teamNM + "VS" + self.teamB.teamNM + ": " + "%.2f : %.2f" + "," + self.teamA.teamNM + "wins!"
-                  %(teamA_performance, teamB_performance))
+            print(self.teamA.teamNM + " VS " + self.teamB.teamNM + ": " + "%.2f VS %.2f"
+                  %(teamA_performance, teamB_performance) + ", " + self.teamA.teamNM + " wins!")
         elif teamA_performance < teamB_performance:
             winner = self.teamB
-            print(self.teamA.teamNM + "VS" + self.teamB.teamNM + ": " + "%.2f : %.2f" + "," + self.teamB.teamNM + "wins!"
-                  %(teamA_performance, teamB_performance))
+            print(self.teamA.teamNM + " VS " + self.teamB.teamNM + ": " + "%.2f VS %.2f"
+                  %(teamA_performance, teamB_performance) + ", " + self.teamB.teamNM + " wins!")
         else:
             self.fight_predict_champion(self)
 
