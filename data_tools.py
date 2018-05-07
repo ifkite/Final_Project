a39f1542210eae604103e3ae02b87c5638432c2d
lemonround1995 <35852552+lemonround1995@users.noreply.github.com>
2018-05-06 21:44:07 -0500

    This file is used to process dataset
    Add docstring to the initial version

--- data_tools.py
+++ data_tools.py
@@ -1,22 +1,46 @@
-"""This module is used to process the data files of players and teams"""
+"""
+This module is used to process the raw NBA data
+As for the detailed description of dataset, please refer to "data_description.pdf "
+"""
 
 import numpy as np
 
 
 def read_dataset(input_file):
-    performance_dataset = np.genfromtxt("nba_data.csv", dtype=np.float, delimiter=",",
+    """Given the input file (NBA data), read and process it.
+
+    :param input_file: The file path
+    :return
+
+    player_performance_dataset: A dict.
+                                Key: Player name
+                                Value: A numpy array whose columns are features such as shot and punishment of a player,
+                                       and whose rows represent records of different matches.
+
+    team_player_time_dataset: A dict.
+                              Key: Team name
+                              Value: A dict with formats like
+                                     {'players': A 1-D numpy array of the names of players of this tream,
+                                      'team_playing_time': the total amount of team playing time in recent matches}
+    """
+    # Extract player performance data
+    performance_dataset = np.genfromtxt(input_file, dtype=np.float, delimiter=",",
                                         skip_header=1, usecols=(2, 3, 4, 5, 6, 7, 8, 9))
-    playerNM_dataset = np.genfromtxt("nba_data.csv", dtype=str, delimiter=",",
+    # Extract player names data
+    playerNM_dataset = np.genfromtxt(input_file, dtype=str, delimiter=",",
                                      skip_header=1, usecols=1)
-    team_dataset = np.genfromtxt("nba_data.csv", dtype=str, delimiter=",",
+    # Extract team names data
+    team_dataset = np.genfromtxt(input_file, dtype=str, delimiter=",",
                                  skip_header=1, usecols=10)
 
+    # Match player's performances data with player's name to create player_performance_dataset
     playerNM_unique = np.unique(playerNM_dataset)
     player_performance_dataset = {}
     for player in playerNM_unique:
         performance = performance_dataset[np.where(playerNM_dataset == player)]
         player_performance_dataset[player] = performance
 
+    # Match roster of players, total amount of team playing time with team name to create team_player_time_dataset
     teamNM_unique = np.unique(team_dataset)
     team_player_time_dataset = {}
     for team in teamNM_unique:
